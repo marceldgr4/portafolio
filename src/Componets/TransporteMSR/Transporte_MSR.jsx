092d1390c9baf36a1e10d53d85fd8ef9462b8e37
hackintosh colombia <100010339+marceldgr@users.noreply.github.com>
2024-11-08 13:34:50 -0500

    proyecto TrasnporteMRS

---
+++ src/Componets/TransporteMSR/Transporte_MSR.jsx
@@ -0,0 +1,19 @@
+import React  from 'react';
+import  Projects  from "../../data/projects.json";
+
+
+export const Transporte_MRS=()=>{
+    return(
+        <h1>TRANSPOTER SMR</h1>
+      <p>
+        La app de transporte SMR para la ciudad de Santa Marta facilita a los usuarios el uso de buses locales,
+        permitiéndoles planificar y optimizar sus viajes por la ciudad. La aplicación ofrece información en tiempo real
+        sobre la ubicación de los buses, rutas disponibles, paradas y horarios estimados de llegada, ayudando a los
+        pasajeros a elegir la mejor opción de transporte. Además, la app permite la gestión de pagos para hacer los
+        viajes más cómodos, brindando a los usuarios la posibilidad de consultar sus compras en línea o en terminales
+        físicos.
+      </p>
+
+    );
+
+}
